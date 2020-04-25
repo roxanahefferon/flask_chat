0a7d801a95691f808d8f4b9e1276b09b983d8e13
roxanahefferon <roxana.hefferon@gmail.com>
2020-04-25 21:35:46 +0000

    added routes to app.py

--- app.py
+++ app.py
@@ -5,7 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World"
+
+    """Main page 
+    with instructions"""
+
+    return "To send a message use: /USERNAME/MESSAGE"
+
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
