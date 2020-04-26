47b6b4c24726b9415898ab9bec5bd3d61a177b08
roxanahefferon <roxana.hefferon@gmail.com>
2020-04-26 19:40:20 +0000

    message textbox

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app.secret_key = "randomstring123"
 messages = []
 
 def add_messages(username, message):
+    
     """Add messages to the `messages` list"""
+
     now = datetime.now().strftime("%H:%M:%S")
     messages_dict = {"timestamp": now, "from": username, "message": message}
     messages.append(messages_dict)
@@ -16,6 +18,7 @@ def add_messages(username, message):
 def index():
 
     """Main page with instructions"""
+
     if request.method == "POST":
         session["username"] = request.form["username"]
 
@@ -24,16 +27,26 @@ def index():
 
     return render_template("index.html")
 
-@app.route("/<username>")
+@app.route("/<username>", methods=["GET", "POST"])
 def user(username):
+
     """Display chat messages"""
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
     return render_template("chat.html", username=username,
         chat_messages=messages)
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
+
     """Create a new message and redirect back to the chat page"""
+
     add_messages(username, message)
     return redirect("/" + username)
 
