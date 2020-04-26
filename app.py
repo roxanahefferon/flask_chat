f4ef9fb7f1823dd84b039dd1cb6238d4a1f96190
roxanahefferon <roxana.hefferon@gmail.com>
2020-04-26 12:07:46 +0000

    storing in a dict

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ app.secret_key = "randomstring123"
 messages = []
 
 def add_messages(username, message):
-    """Add messages to the messages list """
+    """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
-
-def get_all_messages():
-    """Get all of the messages and separate them with a `br`"""
-    return "<br>".join(messages)
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -24,13 +21,13 @@ def index():
 
     if "username" in session:
         return redirect(session["username"])
-            
+
     return render_template("index.html")
 
 @app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
 
 
 @app.route("/<username>/<message>")
